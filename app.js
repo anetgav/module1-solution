10b7ebd372994ac835264c5feeb58c088ac03689
anetgav <anetgav@ya.ru>
2017-12-25 15:40:01 +0200

    ver3

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 (function () {
-  'use strict';
+'use strict';
 
-  angular.module('FirstApp',[])
-  .controller('LunchCheck',LunchCheck);
+angular.module('MyApp', [])
+.controller('LunchCheck', LunchCheck);
 
-  LunchCheck.$inject = ['$scope'];
-  function LunchCheck($scope) {
-    $scope.lunchMenu = "menu";
-    $scope.sayMessage = function (){
-      return "Enjoy!";
-    }
+LunchCheck.$inject = ['$scope'];
+function LunchCheck($scope) {
+var countC = 0;
 
-    $scope.checkIfTooMuch = function(){
-    }
+$scope.checkIfTooMuch = function () {
+  if (!$scope.lunchMenu){
+    $scope.sayMessage = "Enter menu";
+  } else {
+   countC = $scope.lunchMenu.split(',').length;
   }
-})
+
+  if (countC == 0) {
+    $scope.sayMessage = "Enter menu";
+  } else if (countC < 4) {
+    $scope.sayMessage = "Enjoy";
+} else {
+  $scope.sayMessage = "Too much";
+}
+  };
+}
+
+})();
